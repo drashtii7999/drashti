91505ce629235c6cd3382a067db3dfeed5a517c4
Drashti Patel <pateldrashti7999@gmail.com>
2026-07-01 01:28:06 +0530

    29 - 6 lecture

---
+++ 29-6-2026.py
@@ -0,0 +1,28 @@
+#Arithmatic operation
+'''
+num1 = 10
+num2 = 3
+
+print("Addition :" ,num1 + num2 )
+print("Substraction :" ,num1 - num2 )
+print("Multiplication :" ,num1 * num2 )
+print("Division :" ,num1 / num2 )
+print("Modulus :" ,num1 % num2 )
+print("Exponentiations :" ,num1 ** num2 )
+print("Floor division :" ,num1 // num2 )
+'''
+# Assignment operator
+x = 10
+x+=2
+#x = x + 2
+print(x)
+x-=3
+print(x)
+x*=2
+print(x)
+x/=3
+print(x)
+x%=1
+print(x)
+x**=2
+print(x)
