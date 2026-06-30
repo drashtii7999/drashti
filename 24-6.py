5a4b2a74728aa524955072fdd06d05b03f02ec9f
Drashti Patel <pateldrashti7999@gmail.com>
2026-06-30 23:56:34 +0530

    24 - 6 lecture

---
+++ 24-6.py
@@ -0,0 +1,31 @@
+#python basics
+print ("Hellow World!")
+       
+# Add two numbers
+a = 10
+b = 20
+c = a + b
+print(c)
+
+#User input
+name = input("What is your name:")
+print(name)
+
+#User inputs with calculation
+a = int(input("Enter your 1st number:"))
+b = int(input("Enter your 2nd number:"))
+print("Addition",a+b)
+#
+a = int(input("Enter your 1st number:"))
+b = int(input("Enter your 2nd number:"))
+print("Substraction",a-b)
+#
+a = int(input("Enter your 1st number:"))
+b = int(input("Enter your 2nd number:"))
+print("Multiplication",a*b)
+#
+a = int(input("Enter your 1st number:"))
+b = int(input("Enter your 2nd number:"))
+print("Divison",a/b)
+
+
